ad3593be35fd6111c19548c66c195f4e0cef2fbc
MetApogeo <obedgamesps@gmail.com>
2026-08-03 14:11:24 -0600

    first commit

---
+++ src/data/champions.js
@@ -0,0 +1,52 @@
+export const champions = [
+    {
+        id: 'ahri', 
+        name: 'Ahri', 
+        role: 'Mage/Assassin', 
+        thumbnail: 'champions/Ahri_1.png', 
+        banner: 'champions/Ahri_0.jpg', 
+        description: 'Ahri is a nine-tailed fox who uses her charm and magic to manipulate and defeat her enemies.',
+        character: 'champions/Ahri_2.png',
+        site: 'sites/Jonia.webp'
+    },
+    {
+        id: 'yasuo', 
+        name: 'Yasuo', 
+        role: 'Fighter/Assassin',
+        thumbnail: 'champions/yasuo_1.png',
+        banner: 'champions/Yasuo_0.jpg',
+        description: 'Yasuo is a master swordsman who can manipulate the wind to strike down his foes with deadly precision.',
+        character: 'champions/Yasuo_2.png',
+        site: 'sites/Jonia.webp'
+    },
+    {
+        id: 'lux', 
+        name: 'Lux',
+        role: 'Support/Mage',
+        thumbnail: 'champions/Lux_1.png',
+        banner: 'champions/Lux_0.jpg',
+        description: 'Lux is a powerful mage who wields the light to protect her allies and vanquish her enemies.',
+        character: 'champions/Lux_2.png',
+        site: 'sites/Demacia.webp'
+    },
+    {
+        id: 'garen', 
+        name: 'Garen',
+        role: 'Fighter/Tank',
+        thumbnail: 'champions/Garen_1.png',
+        banner: 'champions/Garen_0.jpg',
+        description: 'Garen is a noble warrior who fights for justice and defends his homeland with unwavering courage.',
+        character: 'champions/Garen_2.png',
+        site:'sites/Demacia.webp'
+    },
+    {
+        id: 'jhin', 
+        name: 'Jhin',
+        role: 'Marksman/Assassin',
+        thumbnail: 'champions/Jhin_1.png',
+        banner: 'champions/Jhin_0.jpg',
+        description: 'Jhin is a meticulous and artistic killer who uses his skills to create deadly masterpieces.',
+        character: 'champions/Jhin_2.png',
+        site: 'sites/Jonia.webp'
+    }
+]
